2dc57c0c1f49736e696f743fbc78c5dba518e32d
Devanshi Rangras <rangrasdevanshi@gmail.com>
2025-08-28 01:48:10 +0530

    Update vite.config.js

--- 01_basicreact/vite.config.js
+++ 01_basicreact/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    setupFiles: './setupTests.js',
+    testSetupFiles: './setupTests.js',
     globals: true
   }
 });
